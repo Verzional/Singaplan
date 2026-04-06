93b94c30b6e9dfebf96869f115ae0f3eea7708a4
Verzional <verzional777@gmail.com>
2026-04-06 09:00:18 +0700

    Initial Commit

---
+++ Singaplan/SingaplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingaplanApp.swift
+//  Singaplan
+//
+//  Created by Valentino Manuel Gunawan on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SingaplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
