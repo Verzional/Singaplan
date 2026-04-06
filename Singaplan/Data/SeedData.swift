c520d9f37fb6f630378854c0b5173a9b7c6724df
Verzional <verzional777@gmail.com>
2026-04-06 21:28:30 +0700

    feat: symbol to text change

--- Singaplan/Data/SeedData.swift
+++ Singaplan/Data/SeedData.swift
@@ -13,18 +13,18 @@ struct SeedData {
     static let culinary = CategoryModel(title: "Culinary")
     
     static let categories: [CategoryModel] = [
-        CategoryModel(title: "Mountain", icon: "􁗝", parent: nature),
-        CategoryModel(title: "Marine", icon:"􁎄", parent: nature),
-        CategoryModel(title: "Wildlife", icon: "􀾞", parent: nature),
-        CategoryModel(title: "Artistic", icon: "􀝥", parent: culture),
-        CategoryModel(title: "Ancient", icon: "􂶟", parent: culture),
-        CategoryModel(title: "Festive", icon: "􁓵", parent: culture),
-        CategoryModel(title: "Mega City", icon: "􀝒", parent: metro),
-        CategoryModel(title: "Smart City", icon: "􀖀", parent: metro),
-        CategoryModel(title: "Celestial", icon: "􀆿", parent: supernatural),
-        CategoryModel(title: "Spiritual", icon: "􀲮", parent: supernatural),
-        CategoryModel(title: "Fine Dining", icon:"􀸩", parent: culinary),
-        CategoryModel(title: "Street Food", icon:"􀙬", parent: culinary)
+        CategoryModel(title: "Mountain", icon: "mountain.2", parent: nature),
+        CategoryModel(title: "Marine", icon:"water.waves", parent: nature),
+        CategoryModel(title: "Wildlife", icon: "pawprint", parent: nature),
+        CategoryModel(title: "Artistic", icon: "paintpalette", parent: culture),
+        CategoryModel(title: "Ancient", icon: "laurel.leading.laurel.trailing", parent: culture),
+        CategoryModel(title: "Festive", icon: "party.popper", parent: culture),
+        CategoryModel(title: "Mega City", icon: "building.2", parent: metro),
+        CategoryModel(title: "Smart City", icon: "antenna.radiowaves.left.and.right", parent: metro),
+        CategoryModel(title: "Celestial", icon: "sparkles", parent: supernatural),
+        CategoryModel(title: "Spiritual", icon: "hands.and.sparkles", parent: supernatural),
+        CategoryModel(title: "Fine Dining", icon:"fork.knife", parent: culinary),
+        CategoryModel(title: "Street Food", icon:"flame", parent: culinary)
     ]
     
     static var categoryData: [CategoryModel] {
